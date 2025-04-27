848761912f1f6431024d1bf0171cadda482b7a10
Mohammad Hossein <naghinezhad8228@gmail.com>
2025-04-27 11:40:07 +0330

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -467,355 +467,118 @@
 //   }
 // }
 
-import 'dart:async';
-import 'dart:io' show Platform, sleep;
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
-import 'package:logging/logging.dart';
-import 'package:ndef/ndef.dart' as ndef;
-import 'package:ndef/utilities.dart';
-import 'package:test_new/raw_record_setting.dart';
-import 'package:test_new/text_record_setting.dart';
-import 'package:test_new/uri_record_setting.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
 void main() {
-  Logger.root.level = Level.ALL; // defaults to Level.INFO
-  Logger.root.onRecord.listen((record) {
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
-  runApp(MaterialApp(theme: ThemeData(useMaterial3: true), home: MyApp()));
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  String _platformVersion = '';
-  NFCAvailability _availability = NFCAvailability.not_supported;
-  NFCTag? _tag;
-  String? _result, _writeResult, _mifareResult;
-  late TabController _tabController;
-  List<ndef.NDEFRecord>? _records;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: NfcScreen());
   }
+}
+
+class NfcScreen extends StatefulWidget {
+  const NfcScreen({super.key});
+
+  @override
+  State<NfcScreen> createState() => _NfcScreenState();
+}
+
+class _NfcScreenState extends State<NfcScreen> {
+  String _nfcData = 'No data';
+
+  final TextEditingController _textController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    if (!kIsWeb) {
-      _platformVersion =
-          '${Platform.operatingSystem} ${Platform.operatingSystemVersion}';
-    } else {
-      _platformVersion = 'Web';
-    }
-    initPlatformState();
-    _tabController = TabController(length: 2, vsync: this);
-    _records = [];
-    FlutterNfcKit.tagStream.listen((tag) {
-      setState(() {
-        _tag = tag;
-        print(_tag);
-      });
+
+    NfcManager.instance.isAvailable().then((isAvailable) {
+      if (isAvailable) {
+      } else {
+        setState(() {
+          _nfcData = 'NFC is not available';
+        });
+      }
     });
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    NFCAvailability availability;
-    try {
-      availability = await FlutterNfcKit.nfcAvailability;
-    } on PlatformException {
-      availability = NFCAvailability.not_supported;
-    }
+  void _writeNfcTag() {
+    NfcManager.instance.startSession(
+      onDiscovered: (NfcTag tag) async {
+        Ndef? ndef = Ndef.from(tag);
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+        if (ndef != null && ndef.isWritable) {
+          NdefMessage message = NdefMessage([
+            NdefRecord.createText(_textController.text),
+          ]);
+          try {
+            await ndef.write(message);
+            setState(() {
+              _nfcData = 'Write successful!';
+            });
+          } catch (e) {
+            setState(() {
+              _nfcData = 'Write failed: $e';
+            });
+          }
+        }
 
-    setState(() {
-      // _platformVersion = platformVersion;
-      _availability = availability;
-    });
+        NfcManager.instance.stopSession();
+      },
+    );
+  }
+
+  void _readNfcTag() {
+    NfcManager.instance.startSession(
+      onDiscovered: (NfcTag tag) async {
+        Ndef? ndef = Ndef.from(tag);
+        if (ndef != null) {
+          NdefMessage? message = await ndef.read();
+
+          setState(() {
+            var rawData = message.records.first.payload;
+
+            String textData = String.fromCharCodes(rawData);
+
+            _nfcData = textData.substring(3);
+          });
+        }
+
+        NfcManager.instance.stopSession();
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('NFC Flutter Kit Example App'),
-          bottom: TabBar(
-            tabs: <Widget>[Tab(text: 'Read'), Tab(text: 'Write')],
-            controller: _tabController,
-          ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: <Widget>[
-            Scrollbar(
-              child: SingleChildScrollView(
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const SizedBox(height: 20),
-                      Text(
-                        'Running on: $_platformVersion\nNFC: $_availability',
-                      ),
-                      const SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: () async {
-                          try {
-                            NFCTag tag = await FlutterNfcKit.poll();
-                            setState(() {
-                              _tag = tag;
-                            });
-                            await FlutterNfcKit.setIosAlertMessage(
-                              "Working on it...",
-                            );
-                            _mifareResult = null;
-                            if (tag.standard == "ISO 14443-4 (Type B)") {
-                              String result1 = await FlutterNfcKit.transceive(
-                                "00B0950000",
-                              );
-                              String result2 = await FlutterNfcKit.transceive(
-                                "00A4040009A00000000386980701",
-                              );
-                              setState(() {
-                                _result = '1: $result1\n2: $result2\n';
-                              });
-                            } else if (tag.type == NFCTagType.iso18092) {
-                              String result1 = await FlutterNfcKit.transceive(
-                                "060080080100",
-                              );
-                              setState(() {
-                                _result = '1: $result1\n';
-                              });
-                            } else if (tag.ndefAvailable ?? false) {
-                              var ndefRecords =
-                                  await FlutterNfcKit.readNDEFRecords();
-                              var ndefString = '';
-                              for (int i = 0; i < ndefRecords.length; i++) {
-                                ndefString += '${i + 1}: ${ndefRecords[i]}\n';
-                              }
-                              setState(() {
-                                _result = ndefString;
-                              });
-                            } else if (tag.type == NFCTagType.webusb) {
-                              var r = await FlutterNfcKit.transceive(
-                                "00A4040006D27600012401",
-                              );
-                              print(r);
-                            }
-                          } catch (e) {
-                            setState(() {
-                              _result = 'error: $e';
-                            });
-                          }
-
-                          // Pretend that we are working
-                          if (!kIsWeb) sleep(Duration(seconds: 1));
-                          await FlutterNfcKit.finish(
-                            iosAlertMessage: "Finished!",
-                          );
-                        },
-                        child: Text('Start polling'),
-                      ),
-                      const SizedBox(height: 10),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child:
-                            _tag != null
-                                ? Text(
-                                  'ID: ${_tag!.id}\nStandard: ${_tag!.standard}\nType: ${_tag!.type}\nATQA: ${_tag!.atqa}\nSAK: ${_tag!.sak}\nHistorical Bytes: ${_tag!.historicalBytes}\nProtocol Info: ${_tag!.protocolInfo}\nApplication Data: ${_tag!.applicationData}\nHigher Layer Response: ${_tag!.hiLayerResponse}\nManufacturer: ${_tag!.manufacturer}\nSystem Code: ${_tag!.systemCode}\nDSF ID: ${_tag!.dsfId}\nNDEF Available: ${_tag!.ndefAvailable}\nNDEF Type: ${_tag!.ndefType}\nNDEF Writable: ${_tag!.ndefWritable}\nNDEF Can Make Read Only: ${_tag!.ndefCanMakeReadOnly}\nNDEF Capacity: ${_tag!.ndefCapacity}\nMifare Info:${_tag!.mifareInfo} Transceive Result:\n$_result\n\nBlock Message:\n$_mifareResult',
-                                )
-                                : const Text('No tag polled yet.'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+    return Scaffold(
+      appBar: AppBar(title: Text('NFC Screen')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _textController,
+              decoration: InputDecoration(labelText: 'Enter data to write'),
             ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      ElevatedButton(
-                        onPressed: () async {
-                          if (_records!.isNotEmpty) {
-                            try {
-                              NFCTag tag = await FlutterNfcKit.poll();
-                              setState(() {
-                                _tag = tag;
-                              });
-                              if (tag.type == NFCTagType.mifare_ultralight ||
-                                  tag.type == NFCTagType.mifare_classic ||
-                                  tag.type == NFCTagType.iso15693) {
-                                await FlutterNfcKit.writeNDEFRecords(_records!);
-                                setState(() {
-                                  _writeResult = 'OK';
-                                });
-                              } else {
-                                setState(() {
-                                  _writeResult =
-                                      'error: NDEF not supported: ${tag.type}';
-                                });
-                              }
-                            } catch (e, stacktrace) {
-                              setState(() {
-                                _writeResult = 'error: $e';
-                              });
-                              print(stacktrace);
-                            } finally {
-                              await FlutterNfcKit.finish();
-                            }
-                          } else {
-                            setState(() {
-                              _writeResult = 'error: No record';
-                            });
-                          }
-                        },
-                        child: Text("Start writing"),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return SimpleDialog(
-                                title: Text("Record Type"),
-                                children: <Widget>[
-                                  SimpleDialogOption(
-                                    child: Text("Text Record"),
-                                    onPressed: () async {
-                                      Navigator.pop(context);
-                                      final result = await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) {
-                                            return NDEFTextRecordSetting();
-                                          },
-                                        ),
-                                      );
-                                      if (result != null) {
-                                        if (result is ndef.TextRecord) {
-                                          setState(() {
-                                            _records!.add(result);
-                                          });
-                                        }
-                                      }
-                                    },
-                                  ),
-                                  SimpleDialogOption(
-                                    child: Text("Uri Record"),
-                                    onPressed: () async {
-                                      Navigator.pop(context);
-                                      final result = await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) {
-                                            return NDEFUriRecordSetting();
-                                          },
-                                        ),
-                                      );
-                                      if (result != null) {
-                                        if (result is ndef.UriRecord) {
-                                          setState(() {
-                                            _records!.add(result);
-                                          });
-                                        }
-                                      }
-                                    },
-                                  ),
-                                  SimpleDialogOption(
-                                    child: Text("Raw Record"),
-                                    onPressed: () async {
-                                      Navigator.pop(context);
-                                      final result = await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) {
-                                            return NDEFRecordSetting();
-                                          },
-                                        ),
-                                      );
-                                      if (result != null) {
-                                        if (result is ndef.NDEFRecord) {
-                                          setState(() {
-                                            _records!.add(result);
-                                          });
-                                        }
-                                      }
-                                    },
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        },
-                        child: Text("Add record"),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Text('Result: $_writeResult'),
-                  const SizedBox(height: 10),
-                  Expanded(
-                    flex: 1,
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: List<Widget>.generate(
-                        _records!.length,
-                        (index) => GestureDetector(
-                          child: Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Text(
-                              'id:${_records![index].idString}\ntnf:${_records![index].tnf}\ntype:${_records![index].type?.toHexString()}\npayload:${_records![index].payload?.toHexString()}\n',
-                            ),
-                          ),
-                          onTap: () async {
-                            final result = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return NDEFRecordSetting(
-                                    record: _records![index],
-                                  );
-                                },
-                              ),
-                            );
-                            if (result != null) {
-                              if (result is ndef.NDEFRecord) {
-                                setState(() {
-                                  _records![index] = result;
-                                });
-                              } else if (result is String &&
-                                  result == "Delete") {
-                                _records!.removeAt(index);
-                              }
-                            }
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            ElevatedButton(
+              onPressed: _writeNfcTag,
+              child: Text('Write to NFC'),
             ),
+            ElevatedButton(
+              onPressed: _readNfcTag,
+              child: Text('Read from NFC'),
+            ),
+            SizedBox(height: 20),
+            Text(_nfcData),
           ],
         ),
       ),
